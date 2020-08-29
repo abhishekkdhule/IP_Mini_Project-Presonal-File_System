1a42956b27eeefa8190b0c7925a23ea86445a6db
Mayank Agrawal <agrawalmayank039@gmail.com>
2020-08-29 21:26:37 +0530

    updated login and password connection

--- PHP/config.php
+++ PHP/config.php
@@ -8,9 +8,9 @@ $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
     die("Connection failed".mysqli_connect_error());
 }
-else{
-    echo "Success";
-}
+// else{
+//     echo "Success";
+// }
 ?>
 
 
