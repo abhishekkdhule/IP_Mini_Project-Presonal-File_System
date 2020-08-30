add844db2714cb68211f8e4a329f5f7029dd456d
Mayank Agrawal <agrawalmayank039@gmail.com>
2020-08-30 20:30:37 +0530

    new php page added

--- PHP/login.php
+++ PHP/login.php
@@ -1,7 +1,10 @@
 <?php
 include "config.php";
 // echo "Hello";
-
+// GLOBAL $result1;
+// GLOBAL $sql1;
+// $logo=$_POST["login"];
+// global $login;
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $login=$_POST["login"];
     $password=$_POST["password"];
@@ -13,12 +16,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $result=mysqli_query($conn,$sql);
     if (mysqli_num_rows($result) == 0) { 
         echo '<script> alert("Please check login or password") </script>';
+
         
      } 
      else { 
          echo '<script> alert("Succesfully Login") </script>'; 
+         $sql1="select * from personal_info where Login='$login'";
+        // $result1;
+         $result1=mysqli_query($conn,$sql1);
+         if (mysqli_num_rows($result1) == 0) { 
+             echo '<script>location.href="../HTML/general details.html"</script>';
+             
+             
+        } 
+        else{
+            // $result1=$result1;
+            echo '<script>location.href="../PHP/modified_gd.php"</script>';
+            // echo  "Jayesh Tu kar";
+          }
     }
     
+    
+    
 }
-
+// $result1=$result1;
+// echo 'global["result1"]';
 ?>
