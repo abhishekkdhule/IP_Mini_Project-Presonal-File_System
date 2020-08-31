9c03b252940cbbe73eef986d1f2ec5143b09bad8
Jayesh Deorukhkar <jddeorukhkar@gmail.com>
2020-08-31 11:57:18 +0530

    Research Paper files string and databse connection

--- PHP/get_academic_details.php
+++ PHP/get_academic_details.php
@@ -10,7 +10,7 @@
 
         echo '<hr>';
         echo "Files:<br>";
-        // echo json_encode($_FILES);
+        echo json_encode($_FILES);
         foreach ($_FILES as $key => $value) {
             # code...
             $value = $value["name"]."  ".$value['error'];
@@ -20,6 +20,7 @@
         // Databse Connection
 
         require('config.php');
+
         $data = json_decode($_POST['data']);
         $year = $_POST['year'];
         $dept = $_POST['dept'];
@@ -31,10 +32,29 @@
         $phd_guidance = $_POST['phd_guidance'] == "yes";
         $is_hod = $_POST['hod'] == "yes";
         $received_phd = $_POST['received_phd'] =="yes";
-        foreach ($data as $key ) {
-            echo "$key->title - $key->name_of_journal - $key->impact_factor - $key->paper_name  <br>";
+
+        // Store Papers
+        $paper_dir = "D:/IP Mini Project - Personal File System/UPLOADS/Papers/";
+        for ($i=0; $i < count($data); $i++) { 
+
+            $key = $data[$i];
+            $paper_url = $paper_dir . $key->paper_name;
+            $paper_sql = "INSERT INTO `research_paper`(`Login`, `Title`, `Name_Of_Journal`, `Impact_Factor`, `Publication_Year`, `Paper_Url`)
+                            VALUES ('Jayesh@2812','$key->title','$key->name_of_journal',' $key->impact_factor','$year','$paper_url')";
+            move_uploaded_file($_FILES['paper'.$i]['tmp_name'],$paper_url);
+
+
+            if ($conn->query($paper_sql)){
+                echo "Papers Submitted<br>";
+            }
+            else{
+                echo "Error";
+            }
+            echo "$key->title - $key->name_of_journal - $key->impact_factor - $paper_url  <br>";
         }
+
         // var_dump($data[1]);
+
 
         
 
